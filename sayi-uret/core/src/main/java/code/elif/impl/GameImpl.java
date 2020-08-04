9a82b110c6804c880941ffc5497524584e4078f3
elif akgun <elif.akgun13@gmail.com>
2020-08-04 14:51:22 +0300

    dependency injection style was changed

--- sayi-uret/core/src/main/java/code/elif/impl/GameImpl.java
+++ sayi-uret/core/src/main/java/code/elif/impl/GameImpl.java
@@ -4,6 +4,7 @@ import code.elif.Game;
 import code.elif.NumberGenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 
@@ -13,11 +14,16 @@ public class GameImpl implements Game {
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // == fields ==
+    @Autowired
     private NumberGenerator numberGenerator;
 
-    public void setNumberGenerator(NumberGenerator numberGenerator) {
-        this.numberGenerator = numberGenerator;
-    }
+    private int guessCount = 10;
+    private int number;
+    private int guess;
+    private int smallest;
+    private int biggest;
+    private int remainingGuesses;
+    private boolean validNumberRange = true;
 
     @PostConstruct
     @Override
@@ -29,15 +35,6 @@ public class GameImpl implements Game {
         number = numberGenerator.next();
         log.debug("the number is {}", number);
     }
-
-
-    private int guessCount = 10;
-    private int number;
-    private int guess;
-    private int smallest;
-    private int biggest;
-    private int remainingGuesses;
-    private boolean validNumberRange = true;
 
     // == public methods ==
 
@@ -76,12 +73,12 @@ public class GameImpl implements Game {
 
         checkValidNumberRange();
 
-        if(validNumberRange) {
-            if(guess > number) {
-                biggest = guess -1;
+        if (validNumberRange) {
+            if (guess > number) {
+                biggest = guess - 1;
             }
 
-            if(guess < number) {
+            if (guess < number) {
                 smallest = guess + 1;
             }
         }
