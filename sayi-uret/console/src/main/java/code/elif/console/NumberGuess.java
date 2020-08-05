0ba81f78ef1b03ff52cb6a0155ccf7cd068b1848
elif akgun <elif.akgun13@gmail.com>
2020-08-05 20:01:03 +0300

    application event listtener was added with annotation

--- sayi-uret/console/src/main/java/code/elif/console/NumberGuess.java
+++ sayi-uret/console/src/main/java/code/elif/console/NumberGuess.java
@@ -5,14 +5,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class NumberGuess  {
     private static final Logger log = LoggerFactory.getLogger(NumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info("Container ready for use...");
+    @EventListener
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("start()--> Container ready for use...");
     }
 }
