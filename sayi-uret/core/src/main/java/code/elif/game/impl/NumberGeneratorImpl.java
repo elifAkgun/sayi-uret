26283556a4337f32be73a8ff0e1e16d606664b31
elif akgun <elif.akgun13@gmail.com>
2020-08-06 23:00:50 +0300

    git ignore changed

--- sayi-uret/core/src/main/java/code/elif/game/impl/NumberGeneratorImpl.java
+++ sayi-uret/core/src/main/java/code/elif/game/impl/NumberGeneratorImpl.java
@@ -14,13 +14,16 @@ public class NumberGeneratorImpl implements NumberGenerator {
     // == fields ==
     private final Random random = new Random();
 
-    @Autowired
-    @MaxNumber
-    private int maxNumber ;
+    private final int maxNumber ;
+
+    private final int minNumber;
+
 
     @Autowired
-    @MinNumber
-    private int minNumber;
+    public NumberGeneratorImpl(@MaxNumber int maxNumber, @MinNumber int minNumber) {
+        this.maxNumber = maxNumber;
+        this.minNumber = minNumber;
+    }
 
     // == public methods ==
     @Override
