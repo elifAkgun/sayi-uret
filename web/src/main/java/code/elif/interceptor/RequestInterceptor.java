0cab16d48d7f5b78e04a6c2443410a2975398f7a
elif akgun <elif.akgun13@gmail.com>
2020-08-28 00:12:25 +0300

    local interceptor was added

--- web/src/main/java/code/elif/interceptor/RequestInterceptor.java
+++ web/src/main/java/code/elif/interceptor/RequestInterceptor.java
@@ -1,4 +1,32 @@
 package code.elif.interceptor;
 
-public class RequestInterceptor {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.debug("preHandle method was called. handler : {}",handler);
+        log.debug("Url: {}",request.getRequestURL());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.debug("preHandle method was called. handler : {}",handler);
+        log.debug("Url: {}",request.getRequestURL());
+        log.debug("Model {}", modelAndView.getModel());
+        log.debug("View {}", modelAndView.getView());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.debug("afterCompletion method was called. handler : {}",handler);
+        log.debug("Url: {}",request.getRequestURL());
+    }
 }
